e9d14ea55ae45a805709610af6b5a033a8884392
Aleksey <mc_gra_dy@mail.ru>
2023-07-16 11:05:17 +0300

    добавил отображение в админке

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from catalog.models import Category, Product
 
-# Register your models here.
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'category_description',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
